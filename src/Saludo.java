1bae1b2df403abd0546e03259bdae16ff78dcc2b
Jose Albaladejo <josealbaladejomacia@gmail.com>
2021-01-07 20:56:04 +0100

    Numeros

--- src/Saludo.java
+++ src/Saludo.java
@@ -4,6 +4,12 @@ public class Saludo {
 	public static void main(String[] args) {
 		
 		System.out.println("Hola primero de DAW!");
+		
+		System.out.println("Los números del 1 al 10 son: ");
+		
+		for(int i=1; i<=10; i++) {
+			System.out.println(i);
+		}
 
 	}
 
